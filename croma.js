044835c9405ab49df7afddcd8014aaf01d019450
Shefali <chshefali06@gmail.com>
2023-05-04 18:34:09 +0530

    another commit

--- croma.js
+++ croma.js
@@ -2,6 +2,15 @@
 function changeMode(){
     let mybody = document.body;
     mybody.classList.toggle('mylight')
+}
 
-    
+//For Coupon
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.3'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1'
 }
